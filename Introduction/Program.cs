05e263e93870497621f4b268b3ac015ec0273b47
abdndmn <abdndmn@hotmail.com>
2024-12-16 14:47:43 +0300

    files added

---
+++ Introduction/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Introduction{
+
+    class Program{
+        static void Main(string[] args){
+            Console.WriteLine("Hello everyone! My name is Zeynel Abidin Duman, and I am a trainee at the Patika+ Bootcamp, working towards becoming a Full Stack Developer—a career path that I have always aspired to shape my future on.");
+    }
+    }
+}
